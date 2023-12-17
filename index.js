3d4136f9291c71cb8756712aa121efe009a29160
Adelaide Zhang <adelaidemzhang@gmail.com>
2023-12-17 14:31:32 -0500

    add timestamp microservice

--- index.js
+++ index.js
@@ -19,10 +19,42 @@ var listener = app.listen(process.env.PORT || 3000, function () {
     console.log('Your app is listening on port ' + listener.address().port);
   });
 
+// whoami microservice
 app.get('/api/whoami', (req, res) => {
     res.json({
       ipaddress: req.ip,
       language: req.get("Accept-Language"),
       software: req.get("User-Agent"),
     })
+  });
+
+// timestamp microservice
+app.get("/api/:date?", (req, res) => {
+    try {
+      const input_date = req.params.date;
+      let date;
+  
+      if (input_date === undefined) {
+        date = new Date();
+      }
+      else if (!isNaN(input_date)) {
+        date = new Date(parseInt(input_date));
+      }
+      else if (!isNaN(new Date(input_date).valueOf())) {
+        date = new Date(input_date);
+      }
+      else {
+        res.json({ error: "Invalid Date" });
+        return;
+      }
+
+      res.json({
+        unix: date.valueOf(),
+        utc: date.toUTCString(),
+      });
+  
+    } catch (error) {
+      console.log(error);
+      res.json({ error: "Invalid Date" });
+    }
   });
